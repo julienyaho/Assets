0c7a8df8476f7c4bbec07bd68dfa6bc789bf18a9
julienyaho <julienyaho92@hotmail.com>
2015-12-22 22:17:33 +0000

    code
    code for my car game

---
+++ Scripts/CarController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+	public float carSpeed; // this take control of the car
+	public float maxPos = 1.1f;
+
+	Vector3 position;
+	public UiManager ui;
+
+	// Use this for initialization
+	void Start () {
+		//ui = GetComponent<UiManager> ();
+		position = transform.position; // this is the current position of the the car
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime; //this is the input for the left and right arrow key
+		position.x = Mathf.Clamp (position.x, -1.4f, 1.1f);
+
+		transform.position = position;
+	
+	}
+	
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Enemy Car") {
+		Destroy (gameObject);
+			ui.gameoverActivated();
+
+		}
+	}
+}
---
+++ Scripts/CarSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour {
+	
+	public GameObject[] cars;
+	int CarNo;
+	public float maxPos = 1.1f;
+	public float delayTimer = 0.5f;
+	float timer;
+
+	
+	// Use this for initialization
+	void Start () {
+		timer = delayTimer;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			Vector3 carPos = new Vector3 (Random.Range (-1.3f, 1.1f), transform.position.y, transform.position.z);
+			CarNo = Random.Range (0,4);
+			Instantiate (cars[CarNo], carPos, transform.rotation);
+			timer = delayTimer;
+		}
+
+
+	}
+}
